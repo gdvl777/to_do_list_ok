d222ca60cbee277aa1cd18d862c86c9989e4e24f
gdvl777 <gdvl777@gmail.com>
2023-08-25 21:57:36 -0500

    Update statusUpdates.test.js

--- src/___tests___/statusUpdates.test.js
+++ src/___tests___/statusUpdates.test.js
@@ -1,20 +1,19 @@
-import {updateTaskStatus} from '../statusUpdates.js';
+import { updateTaskStatus } from '../statusUpdates.js';
 import { saveTasks } from '../taskFunctions.js';
 
-
 jest.mock('../taskFunctions', () => ({
-  saveTasks: jest.fn()
+  saveTasks: jest.fn(),
 }));
 
 describe('updateTaskStatus', () => {
   afterEach(() => {
-    saveTasks.mockClear(); 
+    saveTasks.mockClear();
   });
 
   test('It updates the status and order of the task into the list', () => {
     const tasks = [
       { description: 'Tarea 1', completed: false },
-      { description: 'Tarea 2', completed: true }
+      { description: 'Tarea 2', completed: true },
     ];
 
     updateTaskStatus(tasks, 0, true);
@@ -23,6 +22,4 @@ describe('updateTaskStatus', () => {
     expect(saveTasks).toHaveBeenCalledWith(tasks);
   });
 
-  
 });
-
